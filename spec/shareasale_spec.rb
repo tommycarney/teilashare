137230813d4b70922993e37e11c193c772bdc8d7
Thomas Carney <tommycarney@gmail.com>
2021-12-16 18:03:24 +0100

    Support XML and pipe format parsing (#3)

--- spec/shareasale_spec.rb
+++ spec/shareasale_spec.rb
@@ -4,4 +4,26 @@ RSpec.describe Shareasale do
   it 'has a version number' do
     expect(Shareasale::VERSION).not_to be nil
   end
+
+  context 'authentication' do
+    context 'when missing' do
+      it 'raises a MissingCredentialsError error' do 
+        expect { Shareasale.new({}).today_stats }.to raise_error(Shareasale::Errors::MissingCredentialsError)
+      end
+    end
+
+    context 'when incorrect' do
+      let(:authentication) do
+        {
+          token: 'a-very-nice-token-but-sadly-wrong',
+          api_secret: 'very-secret-but-not-correct',
+          affiliate_id: '123456'
+        }
+      end
+
+      it 'raises an InvalidRequestError', vcr: 'invalid-auth' do 
+        expect { Shareasale.new(authentication).today_stats }.to raise_error(Shareasale::Errors::InvalidRequestError)
+      end
+    end
+  end
 end
